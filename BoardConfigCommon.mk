ab8d24f285e8e9d4343eab5a70b067812de65fa8
WildfireDEV <wildfiredev@gmail.com>
2016-06-15 17:01:21 +1000

    zero: Switch to 64bit binder

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
 # big.LITTLE load balancing
 ENABLE_CPUSETS := true
 
